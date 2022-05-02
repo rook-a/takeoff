9f5d4791c0705ec568b973e99cdfcd51b6cc32a9
rook-a <zhernovkov_konst@mail.ru>
2022-05-02 15:46:01 +0300

    feat: добавит показ формы редактирования по клику

--- src/components/contacts-item/contacts-item.tsx
+++ src/components/contacts-item/contacts-item.tsx
@@ -1,24 +1,53 @@
+import { useState } from 'react';
+
 import { ButtonGroup, IconButton, List, ListItem } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CreateIcon from '@mui/icons-material/Create';
+import DoneIcon from '@mui/icons-material/Done';
+import EditContactFrom from '../edit-contact-form/edit-contact-form';
 
 function ContactsItem(): JSX.Element {
+  const [isEditForm, setIsEditForm] = useState(false);
+
+  const handleOpenEditFormClick = () => {
+    setIsEditForm(!isEditForm);
+  };
+
+  const handleCloseEditFormClick = () => {
+    setIsEditForm(!isEditForm);
+  };
+
   return (
     <ListItem sx={{ justifyContent: 'space-between' }}>
-      <List>
-        <ListItem>name: qwerty</ListItem>
-        <ListItem>city: cityname</ListItem>
-        <ListItem>company: companyName</ListItem>
-        <ListItem>phone: 1234</ListItem>
-      </List>
+      {isEditForm ? (
+        <EditContactFrom />
+      ) : (
+        <List>
+          <ListItem>name: qwerty</ListItem>
+          <ListItem>city: cityname</ListItem>
+          <ListItem>company: companyName</ListItem>
+          <ListItem>phone: 1234</ListItem>
+        </List>
+      )}
 
       <ButtonGroup size='medium'>
-        <IconButton
-          edge='end'
-          aria-label='change element'
-          style={{ marginRight: '7px' }}>
-          <CreateIcon />
-        </IconButton>
+        {isEditForm ? (
+          <IconButton
+            onClick={handleCloseEditFormClick}
+            edge='end'
+            aria-label='change element'
+            style={{ marginRight: '7px' }}>
+            <DoneIcon />
+          </IconButton>
+        ) : (
+          <IconButton
+            onClick={handleOpenEditFormClick}
+            edge='end'
+            aria-label='change element'
+            style={{ marginRight: '7px' }}>
+            <CreateIcon />
+          </IconButton>
+        )}
         <IconButton color='error' edge='end' aria-label='delete element'>
           <DeleteIcon />
         </IconButton>
--- src/components/contacts-list/contacts-list.tsx
+++ src/components/contacts-list/contacts-list.tsx
@@ -1,13 +1,10 @@
 import { List } from '@mui/material';
 import ContactsItem from '../contacts-item/contacts-item';
-import EditContactFrom from '../edit-contact-form/edit-contact-form';
 
 function ContactsList(): JSX.Element {
   return (
     <List sx={{ mb: '50px' }}>
       <ContactsItem />
-
-      <EditContactFrom />
     </List>
   );
 }
